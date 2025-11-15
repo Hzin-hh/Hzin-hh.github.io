2633270577f75d135fa565368a28913db3cbf9db
Hzin-hh <2844083920@qq.com>
2025-11-16 01:28:16 +0800

    Site updated: 2025-11-16 01:28:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/26/java/","2025/10/30/mysql/","2025/10/13/web开发/","2025/09/05/计算机组成原理/","2025/09/16/数据结构/"];function toRandomPost(){
+var posts=["2025/10/30/mysql/","2025/10/13/web开发/","2025/10/26/java/","2025/09/16/数据结构/","2025/09/05/计算机组成原理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
