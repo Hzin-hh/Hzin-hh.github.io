84e5434fa0250e9308d5afae3c7830c268b8d3ce
Hzin-hh <2844083920@qq.com>
2025-11-16 17:26:53 +0800

    Site updated: 2025-11-16 17:26:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/30/mysql/","2025/10/13/web开发/","2025/10/26/java/","2025/09/16/数据结构/","2025/09/05/计算机组成原理/"];function toRandomPost(){
+var posts=["2025/11/16/hexo报错/","2025/10/26/java/","2025/10/13/web开发/","2025/10/30/mysql/","2025/09/16/数据结构/","2025/11/16/红黑树c-实现/","2025/09/05/计算机组成原理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
